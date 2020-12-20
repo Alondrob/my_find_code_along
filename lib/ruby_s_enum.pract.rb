0ee2333607e5ca59fba417732f5514e04e481578
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-12-20 15:32:03 -0500

    Done.

---
+++ lib/ruby_s_enum.pract.rb
@@ -0,0 +1,20 @@
+#["Red", "Yellow", "Blue"].each do |color|
+#  puts "There are #{color.length} letters in #{color}"
+#end #=> ["Red", "Yellow", "Blue"]
+
+
+#mathces = []
+#[1, 2, 3, 4, 5].to_a.each do |i|
+ #   mathces << i if i.even? #add i to the matches array if it is even
+#end
+
+#p mathces
+
+arr.select do |number|
+    arr = [1,2,3,4,5]
+    number.even?
+end #=> [2,4]
+
+p arr
+
+
